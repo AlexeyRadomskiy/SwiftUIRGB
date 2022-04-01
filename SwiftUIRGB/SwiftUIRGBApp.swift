b4fbd7a81f3263a30323cf9890051aab6f266f59
Alexey <alexeyradomskiy98@gmail.com>
2022-04-01 23:21:21 +0300

    Initial Commit

---
+++ SwiftUIRGB/SwiftUIRGBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRGBApp.swift
+//  SwiftUIRGB
+//
+//  Created by Alexey on 01.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRGBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
