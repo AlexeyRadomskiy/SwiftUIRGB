b39b55f60945ffb325dbc36c9c125af255975b77
Alexey <alexeyradomskiy98@gmail.com>
2022-04-02 14:46:01 +0300

    Create final ContentView

--- SwiftUIRGB/ContentView.swift
+++ SwiftUIRGB/ContentView.swift
@@ -2,15 +2,46 @@
 //  ContentView.swift
 //  SwiftUIRGB
 //
-//  Created by Alexey on 01.04.2022.
+//  Created by Alexey on 02.04.2022.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var red = 150.0
+    @State private var green = 150.0
+    @State private var blue = 150.0
+    
+    @FocusState private var isInputActive: Bool
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                Color(.systemBrown).ignoresSafeArea()
+                VStack(spacing: 40) {
+                    ColorView(red: red, green: green, blue: blue)
+                    
+                    VStack {
+                        ColorSliderView(sliderValue: $red, color: .red)
+                        ColorSliderView(sliderValue: $green, color: .green)
+                        ColorSliderView(sliderValue: $blue, color: .blue)
+                    }
+                    .frame(height: 120)
+                    .focused($isInputActive)
+                    .toolbar {
+                        ToolbarItemGroup(placement: .keyboard) {
+                            Spacer()
+                            Button("Done") {
+                                isInputActive = false
+                            }
+                        }
+                    }
+                    Spacer()
+                }
+                .padding()
+            }
+        }
     }
 }
 
